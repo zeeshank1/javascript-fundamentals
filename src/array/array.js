c76c2c9abb69a485072f05c309a24d57d654d4b9
Zeeshan Khan <zeeshank1@gmail.com>
2021-04-11 22:57:21 +0530

    FEAT : added additional array method

--- src/array/array.js
+++ src/array/array.js
@@ -10,3 +10,15 @@ var newFruits = fruits.slice(); //slice method returns a new array
 fruits[5] = "woodapple";
 console.log("Old Array after slice() :", fruits);
 console.log("New Array after slice() :", newFruits);
+
+//Array fill method
+var newArray = Array(7).fill("ZEE");
+console.log(
+  "a new array is created with size = 7 and all indexes are set to ZEE -`",
+  newArray
+);
+
+//Array filter method
+var arrayObj = ["a", "b", "c", "d"];
+var newArrayObj = arrayObj.filter((val) => val != "a"); //
+console.log('Get all values from the array except \"a\" :', newArrayObj);
