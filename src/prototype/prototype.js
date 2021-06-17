1f5455795abf504b50ea3430de36becc16c010f6
Zeeshan Khan <zeeshank1@gmail.com>
2021-06-17 17:19:15 +0530

    Feat: added prototype concepts

---
+++ src/prototype/prototype.js
@@ -0,0 +1,20 @@
+
+//Create OBJECT using constructor
+function Bike(color, model) {
+  this.color = color;
+  this.model = model;
+
+  this.getDetails = function () {
+    return "" + `COLOR ${this.color} :: MODEL ${this.model}`;
+  };
+}
+
+Bike.prototype.getDetailsViaPrototype = function(){
+  return `COLOR ${this.color} + MODEL ${this.model}`
+}
+
+var bikeOne = new Bike("Black", "BMW");
+var bikeTwo = new Bike("Orange", "KTM");
+
+console.log("bikeOne -> ", bikeOne.__proto__.getDetailsViaPrototype());
+console.log("Bike -> ", Bike.prototype.getDetailsViaPrototype());
