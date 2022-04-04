d0c56e226933f438add4ae328cc9b7a732918f50
Zeeshan Khan <zeeshank1@gmail.com>
2022-04-04 23:39:34 +0530

    bind() polyfill 2

--- src/misc/polyfill.js
+++ src/misc/polyfill.js
@@ -7,12 +7,15 @@ let printName = function(){
     console.log(this.firstName);
 }
 
+let printNameParam = function(city){
+    console.log(this.firstName + "_"+city);
+}
+
 let newFun = printName.bind(names);
 
 //Create your own myBind method
 //add to global top object like bind()
 Function.prototype.myBind = function(...args){
-
     let obj = this;//refering to the callee of this method i.e., printName
     return function(){
         obj.call(args[0]); // refere to the names object
@@ -21,3 +24,16 @@ Function.prototype.myBind = function(...args){
 
 let newFunMyBind = printName.myBind(names);
 newFunMyBind();
+
+Function.prototype.myBindParams = function(...args){
+    let obj = this;//refering to the callee of this method i.e., printName
+    let params = args.slice(1);
+    return function(){
+        obj.apply(args[0],params); // refere to the names object
+    }
+}
+
+let newFunMyBindParams = printNameParam.myBindParams(names,"indore");
+newFunMyBindParams();
+
+
